75fa5abd1a04012ee62c85934a00655a82513758
Mira Anand <miraanand@comcast.net>
2024-09-24 12:13:45 -0700

    Initial commit

--- main.cpp
+++ main.cpp
@@ -1,12 +1,20 @@
+// COMSC-210 | Lab 9 | Mira Anand
+// Module 5
+// Coded using Visual Studio Code for Mac
 #include <iostream>
 using namespace std;
 
-int main() {
+int main() 
+{
     // complete this code in order to make it output
     // the numbers 1-100
 
-    for ()
+    for (int i = 1; i <= 100; i++)
+    {
         cout << i << " ";
+    }
+
+    cout << endl;
 
     return 0;
 }
